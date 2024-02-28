b797b09ebd7b64e1c8f0c73549e2d9541991b4bd
mastiico <marcinhofilho94@gmail.com>
2024-02-28 19:01:22 -0300

    Reazliado seção 6 com sucesso, implementado a lógica da calculadora e refatorado o código com padrões MVC

---
+++ 6-10/RestFull-6/src/main/java/br/com/marcio/controller/MathController.java
@@ -0,0 +1,25 @@
+package br.com.marcio.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.marcio.exceptions.UnsupportedMathOperationException;
+import br.com.marcio.model.MathModel;
+
+@RestController
+public class MathController extends MathModel {
+
+	@GetMapping("/sum/{numberOne}/{calc}/{numberTwo}")
+	public Double sum(
+			@PathVariable(value = "numberOne") String numberOne,
+			@PathVariable(value = "calc") String calc,
+			@PathVariable(value = "numberTwo") String numberTwo
+		) throws Exception {
+
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)){  
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
+		};
+		return resultado(numberOne, calc, numberTwo);
+	}
+}
---
+++ 6-10/RestFull-6/src/test/java/br/com/marcio/RestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
