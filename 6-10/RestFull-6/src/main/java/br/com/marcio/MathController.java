4341b4e451fd3dee7f4cb42f5abee1c4681154b9
mastiico <marcinhofilho94@gmail.com>
2024-02-28 19:41:19 -0300

    Seção 7 - Concluida | Realizado implementação de person com funcionalidades CRUD e foi utilizado MOCK para teste.

--- 6-10/RestFull-6/src/main/java/br/com/marcio/MathController.java
+++ 6-10/RestFull-6/src/main/java/br/com/marcio/MathController.java
@@ -1,4 +1,4 @@
-package br.com.marcio.controller;
+package br.com.marcio;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
---
+++ 6-10/RestFull-7/src/main/java/br/com/marcio/controllers/PersonController.java
@@ -0,0 +1,51 @@
+package br.com.marcio.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.marcio.models.Person;
+import br.com.marcio.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+		public Person findById(@PathVariable(value = "id") String id) throws Exception {
+		return service.findById(id);
+	}
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+		public List<Person> findAll() {
+		return service.findAll();
+	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+		public Person create(@RequestBody Person person) {
+		return service.create(person);
+	}
+
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+		public Person update(@RequestBody Person person) {
+		return service.update(person);
+	}
+
+	@DeleteMapping(value ="/{id}")
+		public void delete(@PathVariable(value= "id")String id ) {
+		service.delete(id);
+	}
+
+}
