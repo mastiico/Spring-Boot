a310d0932eee02e207a2290f97d3ee4824221cdc
mastiico <marcinhofilho94@gmail.com>
2024-03-01 18:27:33 -0300

    Customizado json na api

---
+++ 10-20/RestFull-12/src/main/resources/db/migration/V2__Populate_Table_person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Cotia - São Paulo | Brasil', 'Marcio', 'Male', 'Filho'),
+	(2, 'Osasco - São Paulo | Brasil', 'Marcio', 'Male', 'Geraldo'),
+	(6, 'Cotia - São Paulo | Brasil', 'Mikaelly', 'Female', 'Paiva Geraldo'),
+	(9, 'Osasco - São Paulo | Brasil', 'Lucas', 'Male', 'Paiva Geraldo'),
+	(10, 'Cotia - São Paulo | Brasil', 'Marcio', 'Male', 'Filho'),
+	(11, 'Chique Chique - Bahia | Brasil', 'Nikola', 'Male', 'Tesla');
