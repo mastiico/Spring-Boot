447fa0ba086b92db0aaabf414dabf5ef243bf1d7
mastiico <marcinhofilho94@gmail.com>
2024-03-02 22:27:23 -0300

    configurado o content para receber e enviar YML

--- 10-20/RestFull-12/src/main/java/br/com/marcio/config/WebConfig.java
+++ 10-20/RestFull-12/src/main/java/br/com/marcio/config/WebConfig.java
@@ -1,13 +1,24 @@
 package br.com.marcio.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import br.com.marcio.serialization.converter.YamlJacksonHttpMessageConverter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     
+    private static final MediaType MEDIA_TYPE_APPLICATION_YML = MediaType.valueOf("application/x-yaml");
+
+    @SuppressWarnings("null")
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlJacksonHttpMessageConverter());
+    } 
     /** Content negotiation via queryParament
     @SuppressWarnings("null") 
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
@@ -30,7 +41,8 @@ public class WebConfig implements WebMvcConfigurer {
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("yaml", MEDIA_TYPE_APPLICATION_YML);
     }
 
 }
--- 10-20/RestFull-12/src/main/java/br/com/marcio/controllers/PersonController.java
+++ 10-20/RestFull-12/src/main/java/br/com/marcio/controllers/PersonController.java
@@ -3,7 +3,7 @@ package br.com.marcio.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import br.com.marcio.util.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,22 +24,22 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 
-	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML,MediaType.APPLICATION_YML})
 		public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
 		return service.findById(id);
 	}
 
-	@GetMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@GetMapping(produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 		public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 
-	@PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 		public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 
-	@PutMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	@PutMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 		public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
